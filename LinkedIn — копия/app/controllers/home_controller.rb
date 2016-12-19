837b1f16ef7583c5908930bd885665032e85b4b6
13LD <tompla96@ukr.net>
2016-12-19 05:59:16 +0200

    -

---
+++ LinkedIn — копия/app/controllers/home_controller.rb
@@ -0,0 +1,34 @@
+
+
+class HomeController < ApplicationController
+  before_action :set_user, except: :front
+  respond_to :html, :js
+
+  def index
+    @post = Post.new
+    @friends = @user.all_following.unshift(@user)
+    @activities = PublicActivity::Activity.where(owner_id: @friends).order(created_at: :desc).paginate(page: params[:page], per_page: 10)
+
+  end
+
+  def about
+    Rails.cache.fetch('users') {User.all}
+    @users = Rails.cache.fetch('users')
+  end
+
+
+  def front
+
+    @activities = PublicActivity::Activity.order(created_at: :desc).paginate(page: params[:page], per_page: 10)
+  end
+
+  def find_friends
+    @friends = @user.all_following
+    @users =  User.where.not(id: @friends.unshift(@user)).paginate(page: params[:page])
+  end
+
+  private
+  def set_user
+    @user = current_user
+  end
+end
