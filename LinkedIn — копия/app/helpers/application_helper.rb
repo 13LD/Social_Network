19571fc8f172026bc41976b28adede6af40c0f93
13LD <tompla96@ukr.net>
2016-12-19 06:00:44 +0200

    -

--- LinkedIn — копия/app/helpers/application_helper.rb
+++
@@ -1,6 +0,0 @@
-
-module ApplicationHelper
-  def belongs_to_user?(resource)
-    resource.user == current_user
-  end
-end
--- LinkedIn — копия/app/helpers/likes_helper.rb
+++
@@ -1,6 +0,0 @@
-
-module LikesHelper
-  def find_like(likeable)
-    likeable.get_likes.where(user: current_user)
-  end
-end
